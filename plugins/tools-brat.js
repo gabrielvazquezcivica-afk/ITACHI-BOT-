ea92ea1726efce83b85551086bfa89692601ee14
Wichi <geraldorincon52@gmail.com>
2025-09-17 11:47:49 -0400

    Actualizar tools-brat.js

--- plugins/tools-brat.js
+++ plugins/tools-brat.js
@@ -1,37 +1,19 @@
-import { sticker} from '../lib/sticker.js'
-import fetch from 'node-fetch'
-import axios from 'axios'
+// API By WillZek 
+import { sticker } from '../lib/sticker.js'
 
-let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-  if (!text) return m.reply(`《★》Ingresa un texto para crear tu sticker\n> *Ejemplo:* ${usedPrefix + command} Copilot`)
+let handler = async(m, { conn, text, args, usedPrefix, command }) => {
 
-  try {
-    const encodedText = encodeURIComponent(text)
-    const stiker = await sticker(null, `https://api.nekorinn.my.id/maker/brat-v2?text=${encodedText}`, global.packname, global.wm)
+if (!text) return m.reply(`《★》Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} ${botname}`);
 
-    conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, {
-      contextInfo: {
-        forwardingScore: 200,
-        isForwarded: false,
-        externalAdReply: {
-          showAdAttribution: false,
-          title: global.wm,
-          body: global.dev,
-          mediaType: 2,
-          sourceUrl: global.imagen1,
-          thumbnail: global.imagen1
-}
-}
-}, { quoted: m})
-} catch (err) {
-    console.error(err)
-    m.reply('❌ Ocurrió un error al generar el sticker.')
-}
+let username = conn.getName(m.sender);
+let stiker = await sticker(null,`https://star-void-api.vercel.app/api/brat?text=${text}`, text, username)
+
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: text, body: username, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: m })
 }
 
-handler.help = ['brat']
-handler.tags = ['sticker']
-handler.command = ['brat']
-handler.estrellas = 3
+handler.help = ['brat'];
+handler.tag = ['sticker'];
+handler.command = ['brat'];
+// handler.estrellas = 3;
 
-export default handler
+export default handler;
