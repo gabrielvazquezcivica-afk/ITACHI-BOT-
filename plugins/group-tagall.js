d08b22edface2c113defb3f508c7b4783e1db2b1
Wichi <geraldorincon52@gmail.com>
2025-09-21 14:03:40 -0400

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -42,7 +42,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args}) =
   messageText += `└───────⭓\n\n𝘚𝘶𝘱𝘦𝘳 𝘉𝘰𝘵 𝘞𝘩𝘢𝘵𝘴𝘈𝘱𝘱 🚩`;
 
   const imageUrl = 'https://cdn-sunflareteam.vercel.app/images/fa68a035ca.jpg';
-  const audioUrl = 'https://cdn.russellxz.click/a8f5df5a.mp3';
+  const audioUrl = 'https://cdn.russellxz.click/3fd9f7de.mp3';
 
   const fkontak = {
     key: {
