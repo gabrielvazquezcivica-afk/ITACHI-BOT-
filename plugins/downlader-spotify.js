3bcca17cf5721ac80ee6f89196c99a1f6af920f2
Wichi <geraldorincon52@gmail.com>
2025-10-15 21:55:52 -0400

    Actualizar downlader-spotify.js

--- plugins/downlader-spotify.js
+++ plugins/downlader-spotify.js
@@ -15,7 +15,7 @@ const handler = async (m, { conn, text, command, usedPrefix}) => {
 
     if (isSpotifyUrl) {
       // 🔽 Descargar directamente desde URL
-      const res = await fetch(`https://api.stellarwa.xyz/dow/spotify?url=${encodeURIComponent(text)}&apikey=stellar-kxcJan1f `)
+      const res = await fetch(`https://api.stellarwa.xyz/dow/spotify?url=${encodeURIComponent(text)}&apikey=stellar-kxcJan1f`)
       const json = await res.json()
 
       if (!json.status ||!json.data ||!json.data.dl_url) {
@@ -25,7 +25,7 @@ const handler = async (m, { conn, text, command, usedPrefix}) => {
       info = json.data
 } else {
       // 🔍 Buscar canción por texto
-      const searchRes = await fetch(`https://api.stellarwa.xyz/search/spotify?query=${encodeURIComponent(text)}&apikey=stellar-kxcJan1f `)
+      const searchRes = await fetch(`https://api.stellarwa.xyz/search/spotify?query=${encodeURIComponent(text)}&apikey=stellar-kxcJan1f`)
       const searchJson = await searchRes.json()
 
       if (!searchJson.status ||!Array.isArray(searchJson.data) || searchJson.data.length === 0) {
