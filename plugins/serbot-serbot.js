0be4c94b5cdbb18181810ac2838e0828e25c8b12
Wichi <geraldorincon52@gmail.com>
2025-10-16 21:02:37 -0400

    Actualizar serbot-serbot.js

--- plugins/serbot-serbot.js
+++ plugins/serbot-serbot.js
@@ -100,10 +100,10 @@ export default handler
 
 export async function JadiBot(options) {
 let { pathJadiBot, m, conn, args, usedPrefix, command } = options
+const mcode = command === 'code' ? true : args[0] && /(--code|code)/.test(args[0].trim()) ? true : args[1] && /(--code|code)/.test(args[1].trim()) ? true : false // Se agregó el chequeo del comando aquí.
 if (command === 'code') {
 command = 'qr'; 
 args.unshift('code')}
-const mcode = args[0] && /(--code|code)/.test(args[0].trim()) ? true : args[1] && /(--code|code)/.test(args[1].trim()) ? true : false
 let txtCode, codeBot, txtQR
 if (mcode) {
 args[0] = args[0].replace(/^--code$|^code$/, "").trim()
