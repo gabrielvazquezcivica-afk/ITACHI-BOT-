105c2c47475186e0bbdd496bcbd319995505e361
Wichi <geraldorincon52@gmail.com>
2025-10-16 22:07:00 -0400

    Actualizar serbot-serbot.js

--- plugins/serbot-serbot.js
+++ plugins/serbot-serbot.js
@@ -129,7 +129,7 @@ const msgRetry = (MessageRetryMap) => { }
 const msgRetryCache = new NodeCache()
 const { state, saveState, saveCreds } = await useMultiFileAuthState(pathJadiBot)
 
-const connectionOptions = {
+//const connectionOptions = {
 logger: pino({ level: "fatal" }),
 printQRInTerminal: false,
 auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})) },
@@ -140,13 +140,13 @@ version: version,
 generateHighQualityLinkPreview: true
 };
 
-/*const connectionOptions = {
+const connectionOptions = {
 printQRInTerminal: false,
 logger: pino({ level: 'silent' }),
 auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})) },
 msgRetry,
 msgRetryCache,
-version: [2, 3000, 1015901307],
+version: [2, 3000, 1025190524],
 syncFullHistory: true,
 browser: mcode ? ['Ubuntu', 'Chrome', '110.0.5585.95'] : ['Bot (Sub Bot)', 'Chrome','2.0.0'],
 defaultQueryTimeoutMs: undefined,
